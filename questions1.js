846a45730223e12416ab05e882cb89185a2d2a30
mohamedaboelfath48-ship-it <mohamedaboelfath48@gmail.com>
2025-12-28 19:34:15 +0200

    Create questions1.js

---
+++ questions1.js
@@ -0,0 +1,12 @@
+var questions = [
+  {
+    question: "What is the capital of Egypt?",
+    choices: ["Cairo", "London", "Paris", "Rome"],
+    correct: 0
+  },
+  {
+    question: "2 + 2 = ?",
+    choices: ["3", "4", "5", "6"],
+    correct: 1
+  }
+];
